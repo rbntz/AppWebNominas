44061086863f3fba73d1b2db9983c1db54ec64ee
rbntz <152593163+rbntz@users.noreply.github.com>
2024-10-30 13:34:19 +0100

    Add files via upload

---
+++ java-doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"laboral.conexion"},{"l":"laboral.controller"},{"l":"laboral.dao"},{"l":"laboral.model"}];updateSearchResults();
